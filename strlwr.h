4fcb45b2fc9bf37fa8d4914bf892afa02ca88ba2
Diamond Bond <diamondbond1@gmail.com>
2020-09-22 00:43:26 +0530

    introduce strlwr.h

---
+++ strlwr.h
@@ -0,0 +1,13 @@
+//  strlwr.h
+// Homebrew strlwr function to get the job done.
+char *strlwr(char *str)
+{
+  unsigned char *p = (unsigned char *)str;
+
+  while (*p) {
+     *p = tolower((unsigned char)*p);
+      p++;
+  }
+
+  return str;
+}
